6debcceac32192c912f7135d6c6e3fa615fcb9f9
Gerahara <gerfersie@gmail.com>
2022-02-12 14:36:02 +0100

    Creating routes Back

---
+++ Back-finddigitaltalent/routes/users.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controllers/UserController')
+
+router.post('/', UserController.register);
+router.put('/', UserController.completingRegistration)
+router.get('/', UserController.getAllUsers);
+
+module.exports = router;
